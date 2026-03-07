bf5fb168b07216a5a0326adc81bc70fe945e2293
karisalim <karimsalim33000@gmail.com>
2026-03-07 11:50:27 +0200

    locators-input-field

--- Module 2/app.py
+++ Module 2/app.py
@@ -11,24 +11,32 @@ with sync_playwright() as p:
     # visit a website
     page.goto("https://bootswatch.com/default/")
    
-    # Locate a Link Element
-    btn= page.get_by_role("button", name="Default button")
+    # # Locate a Link Element
+    # btn= page.get_by_role("button", name="Default button")
 
-    # radio button
-    radio_btn= page.get_by_role("radio", name="Option one is this and that—be sure to include why it's great")
+    # # radio button
+    # radio_btn= page.get_by_role("radio", name="Option one is this and that—be sure to include why it's great")
 
-    # Highlight the element
-    radio_btn.highlight()
+    # # Highlight the element
+    # radio_btn.highlight()
 
-    # Chech box
-    check_box= page.get_by_role("checkbox", name="Default checkbox")
+    # # Chech box
+    # check_box= page.get_by_role("checkbox", name="Default checkbox")
 
-    #highlight the check_box
-    check_box.highlight()
-    check_box.check()
+    # #highlight the check_box
+    # check_box.highlight()
+    # check_box.check()
 
-    # Click the element
-    # btn.click()
+    print("--"*20) ## video Locators-input-field
+
+    email_input = page.get_by_label("Email address")
+    email_input.highlight()
+
+    password_input = page.get_by_label("Password")
+    password_input.highlight()
+
+    # get by placeholder
+    page.get_by_placeholder("Enter email").highlight()
 
     # close the browser
     browser.close()
